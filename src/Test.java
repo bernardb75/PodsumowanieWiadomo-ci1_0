53edb9558b9ce08dc6ec36a275c5577894481e94
Bernard <bernard.babicz@hotmail.com>
2018-07-10 21:25:48 +0200

    Powtórzenie 1 Test

--- src/Test.java
+++ src/Test.java
@@ -1,2 +1,17 @@
+import org.junit.Before;
+
+import static org.junit.Assert.assertEquals;
+
 public class Test {
+    Trojkat trojkat;
+
+    @Before
+    public void Dane(){
+        trojkat=new Trojkat(7,4,5);
+    }
+    @org.junit.Test
+    public void JestProstokatny() {
+        assertEquals(false, trojkat.TrajkatProstokatny());
+    }
+
 }
